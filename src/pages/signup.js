0ac8169420c721f0aa51b50c35c7142983541a31
Rizvi Rishal <rizvirishal1@gmail.com>
2024-03-31 14:14:11 +0530

    built the frontend

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -4,40 +4,45 @@ import "./signup.scss"
 
 function Sign_up(){
     return(
-        <body className="background-gradient">
-            <div class="signup_cont container">
-                    <h1 className="signup_title">Sign Up</h1>
-                <div class="mb-3">
-                  <label for="exampleFormControlInput1" class="form-label">Name</label>
-                  <input  class="form-control" id="exampleFormControlInput1" placeholder="First_name Last_name "/>
-                </div>
-                <div class="mb-3">
-                  <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-                </div>
-                <div class="mb-3">
-                  <label for="exampleFormControlInput1" class="form-label">House name / number</label>
-                  <input  class="form-control" id="exampleFormControlInput1" placeholder=""/>
-                </div>
-                <div class="mb-3">
-                  <label for="exampleFormControlInput1" class="form-label">Locality / street</label>
-                  <input  class="form-control" id="exampleFormControlInput1" placeholder="example road"/>
-                </div>
-                <div class="mb-3">
-                  <label for="exampleFormControlInput1" class="form-label">City</label>
-                  <input  class="form-control" id="exampleFormControlInput1" placeholder=""/>
-                </div>
-                <div class="mb-3">
-                  <label for="exampleFormControlInput1" class="form-label">District</label>
-                  <input  class="form-control" id="exampleFormControlInput1" placeholder=""/>
-                </div>
-                <div class="mb-3">
-                  <label for="exampleFormControlInput1" class="form-label">State</label>
-                  <input  class="form-control" id="exampleFormControlInput1" placeholder=""/>
-                </div>
+        
+            <div class="signup_background">
+              <div class="signup_cont container">
+                      <h1 className="signup_title">Sign Up</h1>
+                  <form action="">
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">Name</label>
+                      <input  class="form-control" id="exampleFormControlInput1" placeholder="First_name Last_name "/>
+                    </div>
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    </div>
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">House name / number</label>
+                      <input  class="form-control" id="exampleFormControlInput1" placeholder=""/>
+                    </div>
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">Locality / street</label>
+                      <input  class="form-control" id="exampleFormControlInput1" placeholder="example road"/>
+                    </div>
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">City</label>
+                      <input  class="form-control" id="exampleFormControlInput1" placeholder=""/>
+                    </div>
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">District</label>
+                      <input  class="form-control" id="exampleFormControlInput1" placeholder=""/>
+                    </div>
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">State</label>
+                      <input  class="form-control" id="exampleFormControlInput1" placeholder=""/>
+                    </div>
+                    <input class="sub_btn btn btn-primary" type="submit" value="Submit"/>
+                  </form>
+              </div>
             </div>
             
-        </body>
+    
     )
 }
 
